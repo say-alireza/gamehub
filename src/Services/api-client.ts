8fbf1f128d00742bb8b68fa7ac5053af783be7d4
say-alireza <a.rahimpanah71@gmail.com>
2025-03-31 23:53:14 +0330

    building game card

--- src/Services/api-client.ts
+++ src/Services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios"
 export default axios.create({
     baseURL:"https://api.rawg.io/api",
     params:{
-        key:"d06bda389cd149418b957da567318f5b"
+        key:"62573053ee58446f84ab9e4357054ac7"
     }
 })
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,16 @@
 
-import { Text} from '@chakra-ui/react'
+import { SimpleGrid, Text} from '@chakra-ui/react'
 import useGames from '../hooks/useGames'
+import GameCard from './GameCard'
 
 const GameGrid = () => {
     const {games,error} = useGames()
    return (
     <>
     {error && <Text>{error}</Text>}
-    <ul>
-        {games.map(game => <li key={game.id}>{game.name}</li>)}
-    </ul>
+    <SimpleGrid columns={{sm:1,md:3,lg:4,xl:5}} spacing={10}>
+        {games.map(game => <GameCard key={game.id} game={game}></GameCard>)}
+    </SimpleGrid>
     </>
   )
 }

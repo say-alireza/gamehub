b395876789d219fb51048246b2ab34fb9ce4dcd4
say-alireza <a.rahimpanah71@gmail.com>
2025-04-02 10:14:59 +0330

    wrapping container  - removing duplications

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      height="100%"
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,21 +1,32 @@
-
-import { SimpleGrid, Skeleton, Text} from '@chakra-ui/react'
-import useGames from '../hooks/useGames'
-import GameCard from './GameCard'
-import GameCardSkeleton from './GameCardSkeleton'
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-    const {games,error,isLoading} = useGames()
-    const Skeletons = [1,2,3,4,5,6]
-   return (
-    <>
-    {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm:1,md:3,lg:4,xl:5}} spacing={10}>
-        {isLoading && Skeletons.map(Skeleton => <GameCardSkeleton key={Skeleton}></GameCardSkeleton>)}
-        {games.map(game => <GameCard key={game.id} game={game}></GameCard>)}
-    </SimpleGrid>
-    </>
-  )
-}
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-export default GameGrid
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 3, lg: 4, xl: 5 }} spacing={10}>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {games.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
+
+export default GameGrid;
+  
